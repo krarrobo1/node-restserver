48a61fabdddbd2f5790516f482ee03b1dec17bb4
Ricardo <ricardoarrobo@gmail.com>
2019-05-10 00:38:25 -0500

    variables de entorno mongo actualizada

--- server/config/config.js
+++ server/config/config.js
@@ -12,10 +12,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ================
 
 let urlDB;
-//let pass = encodeURI('eldev_98');
 
 if (process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/cafe';
 else urlDB = process.env.MONGO_URI;
-//`mongodb+srv://ricardo_arr:${pass}@cafe-zqczv.mongodb.net/test?retryWrites=true`;
+
 
 process.env.URLDB = urlDB;
