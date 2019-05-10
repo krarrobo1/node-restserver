571062fd44a2bdd1b629f599d56d1b5592d5dbb7
Ricardo <ricardoarrobo@gmail.com>
2019-05-10 00:33:49 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,9 +12,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ================
 
 let urlDB;
-let pass = encodeURI('eldev_98');
+//let pass = encodeURI('eldev_98');
 
 if (process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/cafe';
-else urlDB = `mongodb+srv://ricardo_arr:${pass}@cafe-zqczv.mongodb.net/test?retryWrites=true`;
+else urlDB = process.env.MONGO_URI;
+//`mongodb+srv://ricardo_arr:${pass}@cafe-zqczv.mongodb.net/test?retryWrites=true`;
 
 process.env.URLDB = urlDB;
