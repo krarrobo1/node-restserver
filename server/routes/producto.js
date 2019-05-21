2fd47b781917d119754a9e2a3d6c2c6d6bac28b0
Ricardo <ricardoarrobo@gmail.com>
2019-05-20 23:01:23 -0500

    Nuevas funcionalidades, Productos

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -9,20 +9,46 @@ let Categoria = require('../models/categoria');
 
 // Obtener todos los productos
 productoRoute.get('/productos', (req, res) => {
-    // Trae todos los productos
-    Producto.find({})
-        .exec((err, productosDB) => {
-
-        });
-    // populate: usuario categoria
     // paginado
+    let inicio = Number(req.query.desde) || 0;
+    let limite = Number(req.query.limite) || 5;
+    // Trae todos los productos
+    Producto.find({ disponible: true })
+        // populate: usuario categoria
+        .skip(inicio)
+        .limit(limite)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'nombre')
+        .exec((err, productosDB) => {
+            if (err) return res.status(400).json({
+                ok: false,
+                err
+            });
+            res.json({
+                ok: true,
+                productosDB
+            })
+        });
 
 });
 
 // Obtener un producto por ID
 productoRoute.get('/productos/:id', (req, res) => {
+    let id = req.params.id;
     // populate: usuario categoria
-    // paginado
+    Producto.findById(id)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'nombre')
+        .exec((err, productoDB) => {
+            if (err) return res.status(400).json({
+                ok: false,
+                err
+            });
+            res.json({
+                ok: true,
+                producto: productoDB
+            });
+        });
 });
 
 // Crear un nuevo producto
@@ -65,49 +91,74 @@ productoRoute.post('/productos', verificarToken, (req, res) => {
 
     });
 
-
-
-
-
-
     // grabar el usuario
     // grabar una categoria del listado
 });
 
-async() => {
 
-}
 // Actualizar un nuevo producto
 
 productoRoute.put('/productos/:id', (req, res) => {
     // grabar el usuario
+    let id = req.params.id;
+    let body = req.body;
+    Producto.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, productoDB) => {
+        if (err) return res.status(400).json({
+            ok: false,
+            err
+        });
+        if (!productoDB) return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'No se ha encontrado producto con dicho ID'
+            }
+        });
+        res.json({
+            ok: true,
+            producto: productoDB
+        });
+    });
     // grabar una categoria del listado
 });
 
 productoRoute.delete('/productos/:id', (req, res) => {
-    // cambiar atributo disponible a false.
+    let id = req.params.id;
+    Producto.findByIdAndUpdate(id, { disponible: false }, { new: true, runValidators: true }, (err, productoDB) => {
+        if (err) return res.status(400).json({
+            ok: false,
+            err
+        });
+        if (!productoDB) return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'No se ha encontrado producto con dicho ID'
+            }
+        });
+        res.json({
+            ok: true,
+            producto: productoDB
+        });
+    });
 });
 
+//Buscar producto
+productoRoute.get('/productos/buscar/:termino', verificarToken, (req, res) => {
+    let termino = req.params.termino;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let regexp = new RegExp(termino, 'i');
+    Producto.find({ nombre: regexp })
+        .populate('categoria', 'nombre')
+        .exec((err, productos) => {
+            if (err) return res.status(400).json({
+                ok: false,
+                err
+            });
+            res.json({
+                ok: true,
+                productos
+            });
+        });
+});
 module.exports = {
     productoRoute
 }
